086203d1c67f605b57d32d3dec18f7000a7593ff
bbachdev <bbach7139@gmail.com>
2023-02-28 20:12:45 -0600

    Added auxiliary pages + temp Settings page

---
+++ src/pages/channel/[channelName]/settings.tsx
@@ -0,0 +1,9 @@
+import AuthContainer from "@/components/AuthContainer"
+
+export default function ChannelSettings() {
+  return (
+    <AuthContainer>
+      <div>Channel Settings Home</div>
+    </AuthContainer>
+  )
+}
