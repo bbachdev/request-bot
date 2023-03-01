47c84a3600c70a2c537b5dd3057e0807d045fccb
bbachdev <bbach7139@gmail.com>
2023-03-01 11:39:00 -0600

    Added Twitch OAuth token refresh

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,46 @@
 import NextAuth from "next-auth"
 import TwitchProvider from "next-auth/providers/twitch"
 
+async function refreshTwitchAccessToken(token : any) {
+  try{
+    const reqUrl = 'https://id.twitch.tv/oauth2/token?'+
+    new URLSearchParams({
+      grant_type: 'refresh_token',
+      refresh_token: ''+token.refreshToken,
+      client_id: ''+process.env.TWITCH_CLIENT_ID,
+      client_secret: ''+process.env.TWITCH_CLIENT_SECRET,
+    })
+
+    const response = await fetch(reqUrl, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      method: "POST",
+    })
+
+    const refreshedTokens = await response.json()
+
+    if (!response.ok) {
+      throw refreshedTokens
+    }
+
+    return {
+      ...token,
+      accessToken: refreshedTokens.access_token,
+      accessTokenExpires: Date.now() + refreshedTokens.expires_at * 1000,
+      refreshToken: refreshedTokens.refresh_token ?? token.refreshToken, // Fall back to old refresh token
+    }
+  }catch(e){
+    console.log(e)
+
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    }
+  }
+
+}
+
 export const authOptions = {
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
@@ -13,17 +53,24 @@ export const authOptions = {
     signIn: '/',
   },
   callbacks: {
-    async jwt({ token, account = {}, profile } : any) {
-      if(account.provider && !token[account.provider]){
-        token[account.provider] = {}
+    async jwt({ token, account = {}, user, profile } : any) {
+      if (account && user) {
+        if(account.access_token){
+          token.accessToken = account.access_token
+        }
+        if(account.refresh_token){
+          token.refreshToken = account.refresh_token
+        }
       }
-      if(account.access_token){
-        token[account.provider].accessToken = account.access_token
+
+      // Return previous token if the access token has not expired yet
+      if (Date.now() > token.exp) {
+        return token
       }
-      if(account.refresh_token){
-        token[account.provider].refreshToken = account.refresh_token
-      }
-      return token
+
+      // Access token has expired, try to update it
+      let refreshedTokens = await refreshTwitchAccessToken(token)
+      return refreshedTokens
     }
   }
 }
--- src/pages/channel/[channelName]/index.tsx
+++ src/pages/channel/[channelName]/index.tsx
@@ -1,5 +1,7 @@
 import AuthLayout from "@/components/AuthContainer"
+import { authOptions } from '@/pages/api/auth/[...nextauth]'
 import { UserData } from "@/types/TwitchTypes";
+import { getServerSession } from "next-auth/next"
 import { getToken } from "next-auth/jwt";
 import Image from "next/image";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next/types";
@@ -8,8 +10,8 @@ export default function Channel({ channelInfo } : InferGetServerSidePropsType<ty
   return (
     <AuthLayout>
       {channelInfo.id && 
-        <div className={'flex justify-center items-center text-4xl'}>
-          <Image alt="Channel profile picture" src={channelInfo.profile_image_url} height={60} width={60} className={'rounded-full mr-4'}></Image> {channelInfo.display_name}
+        <div className={'flex justify-center items-center text-3xl'}>
+          <Image alt="Channel profile picture" src={channelInfo.profile_image_url} height={45} width={45} className={'rounded-full mr-4'}></Image> {channelInfo.display_name}
         </div>
       }
     </AuthLayout>
@@ -17,15 +19,17 @@ export default function Channel({ channelInfo } : InferGetServerSidePropsType<ty
 }
 
 //TODO: Begin connection to app db and retrieve songlist + queue
-export const getServerSideProps: GetServerSideProps<{channelInfo : UserData}> = async ({query, req}) => {
+export const getServerSideProps: GetServerSideProps<{channelInfo : UserData}> = async ({query, req, res}) => {
+  const session = await getServerSession(req, res, authOptions)
+  console.log(session)
+
   const { channelName } = query;
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
 
-  if(token){
-    const twitchProps : any = token['twitch']
-    console.log(twitchProps!.accessToken)
+  if(session && token){
+    console.log(token.accessToken)
     console.log(process.env.TWITCH_CLIENT_ID)
-    const res = await fetch(`https://api.twitch.tv/helix/users?login=`+channelName, { method: 'GET', headers: {"Authorization" : 'Bearer '+twitchProps!.accessToken, "Client-Id":''+process.env.TWITCH_CLIENT_ID}})
+    const res = await fetch(`https://api.twitch.tv/helix/users?login=`+channelName, { method: 'GET', headers: {"Authorization" : 'Bearer '+token.accessToken, "Client-Id":''+process.env.TWITCH_CLIENT_ID}})
     const twitchRes : any = await res.json()
     console.log(twitchRes)
     const singleUser = twitchRes.data[0]
