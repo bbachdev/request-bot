e678a0d2394cb6b5cb3981ae641cdabbea7951ef
Barry Bach <bbach7139@gmail.com>
2024-09-10 09:26:28 -0500

    Initial creation of Twitch OAuth function

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,1 +1,11 @@
 'use server'
+import { Twitch } from "arctic";
+import { generateState } from "arctic";
+
+export async function signInTwitch() {
+  const twitch = new Twitch(process.env.TWTICH_CLIENT_ID!, process.env.TWITCH_CLIENT_SECRET!, process.env.TWITCH_REDIRECT_URI!);
+  const state = generateState();
+  const scopes = ["activity:write", "read"];
+  const url = twitch.createAuthorizationURL(state, scopes);
+
+}
