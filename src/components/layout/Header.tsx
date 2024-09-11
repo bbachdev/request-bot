2298ea02f7acf4b012d02da2393d0c7d91209126
Barry Bach <bbach7139@gmail.com>
2024-09-10 20:45:35 -0500

    Add Sign Out button to Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,20 @@
-export default function Header() {
+import { validateRequest } from '@/lib/lucia';
+import { Button } from '../ui/button';
+import { signOut } from '@/actions/auth';
+
+export default async function Header() {
+  const { user } = await validateRequest();
+
   return (
     <header className={`flex p-4 shadow-lg`}>
-      Header
+      <div>
+        Header
+      </div>
+      { user && (
+        <div>
+          <Button onClick={() => signOut()}>Sign Out</Button>
+        </div>
+      )}
     </header>
   )
 }
