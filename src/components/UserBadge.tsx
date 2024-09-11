605bf1356d7a3e686c65b34279704fd2bf4e647b
Barry Bach <bbach7139@gmail.com>
2024-09-11 11:06:35 -0500

    Configure logged-in section in Header

--- src/components/UserBadge.tsx
+++ src/components/UserBadge.tsx
@@ -1,7 +1,10 @@
 'use client'
 import { signOut } from '@/actions/auth'
-import { Button } from './ui/button'
+import { MdArrowDropDown, MdAccountCircle } from "react-icons/md";
+import { PiSignOutBold } from "react-icons/pi";
 import type { User } from "lucia";
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger, DropdownMenuItem } from './ui/dropdown-menu';
 
 interface UserBadgeProps {
   user: User | null
@@ -11,9 +14,20 @@ export default function UserBadge( {user}: UserBadgeProps) {
   return (
     <>
       {user && (
-        <div>
-          <Button onClick={() => signOut()}>Sign Out</Button>
-        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger className={`flex flex-row gap-2 items-center`}>
+              <Avatar>
+                <AvatarImage src={user.profile_image_url} />
+                <AvatarFallback>{user.display_name?.charAt(0).toUpperCase()}</AvatarFallback>
+              </Avatar>
+              <p className={`font-bold`}>{user.display_name}</p>
+              <MdArrowDropDown size={30}/>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem><span className={`flex flex-row gap-2 items-center`}><MdAccountCircle size={14}/>My Channel</span></DropdownMenuItem>
+            <DropdownMenuItem onClick={() => signOut()}><span className={`flex flex-row gap-2 items-center`}><PiSignOutBold size={14}/>Sign Out</span></DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       )}
     </>
   )
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,6 +3,7 @@ import UserBadge from '../UserBadge';
 
 export default async function Header() {
   const { user } = await validateRequest();
+  console.log("User: ", user)
 
   return (
     <header className={`flex p-4 shadow-lg items-center`}>
