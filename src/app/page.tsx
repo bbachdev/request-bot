3ab8920dd79c11df42f96e997ef9fa0e34276e6a
Barry Bach <bbach7139@gmail.com>
2024-09-10 20:55:06 -0500

    Add onClick for Sign In button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+'use client'
+import { signInTwitch } from '@/actions/auth';
 import { Button } from '@/components/ui/button';
 import { FaTwitch } from "react-icons/fa6";
 
 export default function Home() {
   return (
     <div className={`flex-grow flex items-center justify-center`}>
-      <Button className={`bg-[#9146ff] hover:bg-[#9146ff]/90 gap-2`}><FaTwitch size={20}/>Sign In with Twitch</Button>
+      <Button className={`bg-[#9146ff] hover:bg-[#9146ff]/90 gap-2`} onClick={() => signInTwitch()}><FaTwitch size={20}/>Sign In with Twitch</Button>
     </div>
   );
 }
